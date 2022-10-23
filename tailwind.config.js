232f778f4bc5692edcddc37b6e4a16b3ca21c87e
iamzilfa <zilcyam@gmail.com>
2022-10-23 22:37:06 +0200

    all

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,11 @@ module.exports = {
         lightPurple: "#7A73FF",
         grayPara: "#ADBDCC",
         grayFooter: "#f6f9fc",
-
+        grayHover: "hsla(0,0%,100%,0.4),",
+        textHover :"#142541",
+        violet: "#a960ee",
+        darkRed: "#ff333d",
+        aquamarine: "#90e0ff"
       },
       translate: {
         "55%": "55%"
@@ -32,9 +36,27 @@ module.exports = {
       },
       gridTemplateColumns:{
         20 : "112px 141px 58px 380x",
+        30: "323px 501px 256px",
       },
       gridTemplateRows:{
         10: "107px 87px 125px 185px",
+        20: "86px 201px 126px 425px"
+      },
+      boxShadow:{
+        cardShadowXSmall:" 0 2px 5px -1px rgba(50,50,93,0.25),0 1px 3px -1px rgba(0,0,0,0.3)",
+        cardShadowSmall: "0 6px 12px -2px rgba(50,50,93,0.25),0 3px 7px -3px rgba(0,0,0,0.3)",
+        cardShadowMedium: "0 13px 27px -5px rgba(50,50,93,0.25),0 8px 16px -8px rgba(0,0,0,0.3)",
+        cardShadowLarge: "0 30px 60px -12px rgba(50,50,93,0.25),0 18px 36px -18px rgba(0,0,0,0.3)",
+        cardShadowLargeInset: "inset 0 30px 60px -12px rgba(50,50,93,0.25),inset 0 18px 36px -18px rgba(0,0,0,0.3)",
+        cardShadowXLarge: "0 50px 100px -20px rgba(50,50,93,0.25),0 30px 60px -30px rgba(0,0,0,0.3)",
+        cardShadowXSMallMargin: "2px",
+        cardShadowSmallMargin: "8px",
+        cardShadowMediumMargin: '16px',
+        cardShadowLargeMargin: "32px",
+        cardShadowXLargeMargin: "48px",
+      },
+      backgroundImage:{
+        'globalImage' :"url('/img/globe.png')"
       }
     },
   },
